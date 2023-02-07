6ba1aea8f0e14127fb3377fe57af2a76aca25de0
Vincent Darcq <vincentdarcq@MacBook-Air-de-Vincent.local>
2023-02-07 15:49:21 +0100

    exercice 5 vincent

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from '../shared/service/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, OnDestroy {
   public users!: string[];
+  public subscription: Subscription
 
-  constructor() // il faut probablement injecter un service ici !
+  constructor(
+    private userService: UserService
+  ) // il faut probablement injecter un service ici !
   {}
-
+  
   ngOnInit() {
-    // il faut initialiser les users ici avec le service
+    this.subscription = this.userService.users.subscribe((users: string[]) => {
+      this.users = users;
+    })
+  }
+
+  ngOnDestroy(): void {
+    if(this.subscription){this.subscription.unsubscribe();}
   }
 }
